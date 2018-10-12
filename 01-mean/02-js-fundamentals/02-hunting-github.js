716b083c0243ff500d96c0cd859989cee2111362
Zach R <zachkery999@gmail.com>
2018-10-12 15:23:23 -0500

    Completed hunting github
    boom

--- 01-mean/02-js-fundamentals/02-hunting-github.js
+++ 01-mean/02-js-fundamentals/02-hunting-github.js
@@ -1,1 +1,12 @@
 // 1 hour
+
+$(function () {
+    $.get("https://api.github.com/users/ZVR999", displayName)
+    function displayName(data) {
+        console.log(data.name);
+        $('#button').click(function(){
+            $('#name').append(data.name);
+        })
+    }
+
+});
